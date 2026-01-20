d8e44a2cde857b0d080001a2c60a94468f7a6eb5
PierreBarrat <p.barrat@live.fr>
2026-01-20 10:17:57 +0100

    improve read_graph: can parse files like J 1 2 A C value (adabmDCA)

--- src/IO.jl
+++ src/IO.jl
@@ -2,14 +2,25 @@
 ##################### Reading PottsGraph #####################
 #============================================================#
 
-# PottsGraph(file::AbstractString) = read_graph_extended(file)
 """
     read_graph
     read_potts_graph
 
 Read `PottsGraph` object from a file.
 """
-read_graph(file::AbstractString, T=FloatType) = read_graph_extended(file, T)
+function read_graph(file::AbstractString, T=FloatType)
+    first_line = readline(file)
+    format = infer_format_from_line(first_line)
+    if format == :numerical
+        @debug "Numerical format detected"
+        return read_graph_numerical(file, T)
+    elseif format == :symbolic
+        @debug "Symbolic format detected"
+        return read_graph_symbolic(file, T)
+    else
+        throw(ArgumentError("Unknown format for first line of $file\n $(first_line)"))
+    end
+end
 """
     read_potts_graph
 
@@ -17,21 +28,70 @@ Alias for `read_graph`.
 """
 read_potts_graph = read_graph
 
-function read_graph_extended(file, T=FloatType)
+function read_graph_symbolic(file, T=FloatType)
+    ## Go through file twice: first to get L, the second to store parameters
+    # Note that this function only works for amino acids --> q = 21
+    L = 0
+    q = 21
+    min_idx = Inf
+    index_style = 1
+    for (n, line) in enumerate(eachline(file))
+        if !is_valid_line(line, :symbolic)
+            throw(ArgumentError("""
+                Format problem with line $n in $file
+                Expected format `J i j a b` or `h i a`, with amino acid symbols.\
+                Instead $line"""))
+        end
+        if !isempty(line) && line[1] == 'h'
+            i, a, val = parse_field_line_symbolic(line, T)
+            L = max(L, i)
+            min_idx = min(min_idx, i)
+        end
+    end
+    if min_idx != 1 && min_idx != 0
+        throw(
+            ArgumentError("Issue with indexing in $file: smallest index found is $min_idx")
+        )
+    end
+    index_style = (min_idx == 0 ? 0 : 1)
+    if index_style == 0
+        L += 1
+    end
+    @debug "Index style: $index_style"
+    @debug L, q
+
+    g = PottsGraph(L, q, T)
+    for line in eachline(file)
+        if line[1] == 'J'
+            i, j, a, b, val = parse_coupling_line_symbolic(line, T)
+            index_style == 0 && (i += 1; j += 1)
+            g.J[a, b, i, j] = val
+            g.J[b, a, j, i] = val
+        elseif line[1] == 'h'
+            i, a, val = parse_field_line_symbolic(line, T)
+            index_style == 0 && (i += 1)
+            g.h[a, i] = val
+        end
+    end
+
+    return g
+end
+
+function read_graph_numerical(file, T=FloatType)
     ## Go through file twice: first to get L and q, the second to store parameters
     q = 0
     L = 0
     min_idx = Inf
     index_style = 1
     for (n, line) in enumerate(eachline(file))
-        if !is_valid_line(line)
-            error("""
+        if !is_valid_line(line, :numerical)
+            throw(ArgumentError("""
                 Format problem with line $n in $file
-                Expected format `J i j a b` or `h i a`. Instead $line
-                """)
+                Expected format `J i j a b` or `h i a` (with numerical symbols).
+                Instead $line"""))
         end
         if !isempty(line) && line[1] == 'h'
-            i, a, val = parse_field_line(line, T)
+            i, a, val = parse_field_line_numerical(line, T)
             if i > L
                 L = i
             end
@@ -44,7 +104,9 @@ function read_graph_extended(file, T=FloatType)
         end
     end
     if min_idx != 1 && min_idx != 0
-        error("Issue with indexing in $file: smallest index found is $min_idx")
+        throw(
+            ArgumentError("Issue with indexing in $file: smallest index found is $min_idx")
+        )
     end
     index_style = (min_idx == 0 ? 0 : 1)
     if index_style == 0
@@ -55,12 +117,12 @@ function read_graph_extended(file, T=FloatType)
     g = PottsGraph(L, q, T)
     for line in eachline(file)
         if line[1] == 'J'
-            i, j, a, b, val = parse_coupling_line(line, T)
+            i, j, a, b, val = parse_coupling_line_numerical(line, T)
             index_style == 0 && (i += 1; j += 1; a += 1; b += 1)
             g.J[a, b, i, j] = val
             g.J[b, a, j, i] = val
         elseif line[1] == 'h'
-            i, a, val = parse_field_line(line, T)
+            i, a, val = parse_field_line_numerical(line, T)
             index_style == 0 && (i += 1; a += 1)
             g.h[a, i] = val
         end
@@ -69,22 +131,61 @@ function read_graph_extended(file, T=FloatType)
     return g
 end
 
-function is_valid_line(line)
-    if isnothing(match(r"J [0-9]+ [0-9]+ [0-9]+ [0-9]+", line)) &&
-        isnothing(match(r"h [0-9]+ [0-9]+", line))
-        return false
-    else
-        return true
-    end
+let
+    aas = prod(symbols(aa_alphabet))
+    patterns = Dict(
+        :numerical => Regex.(["J [0-9]+ [0-9]+ [0-9]+ [0-9]+", "h [0-9]+ [0-9]+"]),
+        :symbolic => Regex.(["J [0-9]+ [0-9]+ [$(aas)]+ [$(aas)]+", "h [0-9]+ [$(aas)]"]),
+    )
+    global get_line_patterns() = patterns
 end
-function parse_field_line(line, T)
+
+function infer_format_from_line(line)
+    pattern_dict = get_line_patterns()
+    for (name, patterns) in pattern_dict
+        if !isnothing(match(patterns[1], line)) || !isnothing(match(patterns[2], line))
+            return name
+        end
+    end
+    return nothing
+end
+function is_valid_line(line, format::Symbol)
+    patterns = get_line_patterns()[format]
+    return !isnothing(match(patterns[1], line)) || !isnothing(match(patterns[2], line))
+end
+
+# function is_valid_line(line)
+#     if isnothing(match(r"J [0-9]+ [0-9]+ [0-9]+ [0-9]+", line)) &&
+#         isnothing(match(r"h [0-9]+ [0-9]+", line))
+#         return false
+#     else
+#         return true
+#     end
+# end
+function parse_field_line_symbolic(line, T)
+    s = split(line, " ")
+    i = parse(Int, s[2])
+    a = Int(aa_alphabet(s[3][1]))
+    val = parse(T, s[4])
+    return i, a, val
+end
+function parse_field_line_numerical(line, T)
     s = split(line, " ")
     i = parse(Int, s[2])
     a = parse(Int, s[3])
     val = parse(T, s[4])
     return i, a, val
 end
-function parse_coupling_line(line, T)
+function parse_coupling_line_symbolic(line, T)
+    s = split(line, " ")
+    i = parse(Int, s[2])
+    j = parse(Int, s[3])
+    a = Int(aa_alphabet(s[4][1]))
+    b = Int(aa_alphabet(s[5][1]))
+    val = parse(T, s[6])
+    return i, j, a, b, val
+end
+function parse_coupling_line_numerical(line, T)
     s = split(line, " ")
     i = parse(Int, s[2])
     j = parse(Int, s[3])
@@ -103,26 +204,47 @@ end
 
 Write parameters of `g` to `file` using the format `J i j a b value`.
 """
-function write(file::AbstractString, g::PottsGraph; sigdigits=5, index_style=1)
-    return write_graph_extended(file, g, sigdigits, index_style)
+function write(
+    file::AbstractString, g::PottsGraph; sigdigits=5, index_style=1, format=:numerical
+)
+    return write_graph_extended(file, g, sigdigits, index_style, format)
 end
 
-function write_graph_extended(file::AbstractString, g::PottsGraph, sigdigits, index_style)
+function write_graph_extended(
+    file::AbstractString, g::PottsGraph, sigdigits, index_style, format
+)
     @argcheck index_style == 0 || index_style == 1 "Got `index_style==`$(index_style)"
+    @argcheck format in (:numerical, :symbolic) "Got format=$format"
     L, q = size(g)
     open(file, "w") do f
         for i in 1:L, j in (i + 1):L, a in 1:q, b in 1:q
             val = round(g.J[a, b, i, j]; sigdigits)
-            if index_style == 0
+            if format == :symbolic
+                a, b = aa_alphabet.([a, b])
+            end
+            if index_style == 0 && format == :numerical
                 write(f, "J $(i-1) $(j-1) $(a-1) $(b-1) $val\n")
+            elseif index_style == 0 && format == :symbolic
+                write(f, "J $(i-1) $(j-1) $(a) $(b) $val\n")
             elseif index_style == 1
                 write(f, "J $i $j $a $b $val\n")
+            else
+                throw(
+                    ArgumentError(
+                        "Invalid arguments index_style=$(index_style) & format=$(format)"
+                    ),
+                )
             end
         end
         for i in 1:L, a in 1:q
             val = round(g.h[a, i]; sigdigits)
-            if index_style == 0
+            if format == :symbolic
+                a = aa_alphabet(a)
+            end
+            if index_style == 0 && format == :numerical
                 write(f, "h $(i-1) $(a-1) $val\n")
+            elseif index_style == 0 && format == :symbolic
+                write(f, "h $(i-1) $(a) $val\n")
             elseif index_style == 1
                 write(f, "h $i $a $val\n")
             end
--- test/pottsgraph/test.jl
+++ test/pottsgraph/test.jl
@@ -1,3 +1,38 @@
+@testset "IO" begin
+    numerical_graph_file_1 = joinpath(dirname(@__FILE__), "test_numerical_1.pottsgraph")
+    g1 = read_graph(numerical_graph_file_1)
+    @test isa(g1, PottsGraph)
+    L, q = size(g1)
+    @test L == 2
+    @test q == 21
+    for i in 1:L, j in (i + 1):L, a in 1:q, b in 1:q
+        if g1.J[a, b, i, j] == 0 # random graph, so technically it is a rand based test
+            @test false
+            break
+        end
+        g1.h[a, i] == 0 && (@test false; break)
+    end
+
+    numerical_graph_file_0 = joinpath(dirname(@__FILE__), "test_numerical_0.pottsgraph")
+    g0 = read_graph(numerical_graph_file_0)
+    @test g0.J == g1.J
+    @test g0.h == g1.h
+
+    symbolic_graph_file_1 = joinpath(dirname(@__FILE__), "test_symbolic_1.pottsgraph")
+    g2 = read_graph(symbolic_graph_file_1)
+    @test isa(g2, PottsGraph)
+    L, q = size(g2)
+    @test L == 2
+    @test q == 21
+
+    symbolic_graph_file_0 = joinpath(dirname(@__FILE__), "test_symbolic_0.pottsgraph")
+    g3 = read_graph(symbolic_graph_file_0)
+    @test g3.J == g2.J
+    @test g3.h == g2.h
+
+    # I could (should?) add a test where lines of the file are shuffled?
+end
+
 @testset "Gauge change" begin
     g = PottsGraph(5, 3; init=:rand)
     PottsEvolver.set_gauge!(g, :zero_sum)
